c16c8d0f57872db133f3fd730c9f5d7208fdd8d2
michael1309 <michael.lehning@lehning.de>
2018-04-25 18:56:40 +0200

    Rebase from 'rpm/kinetic/sick_scan'

--- include/sick_scan/sick_scan_common_tcp.h
+++ include/sick_scan/sick_scan_common_tcp.h
@@ -44,54 +44,96 @@
 
 #include "sick_scan_common.h"
 #include "sick_generic_parser.h"
-
+#include "template_queue.h"
 namespace sick_scan
 {
 
 class SickScanCommonTcp : public SickScanCommon
-{
+    {
 public:
-  SickScanCommonTcp(const std::string &hostname, const std::string &port, int &timelimit, SickGenericParser* parser);
-  virtual ~SickScanCommonTcp();
+    static void disconnectFunctionS(void *obj);
 
-protected:
-  virtual int init_device();
-  virtual int close_device();
+    SickScanCommonTcp(const std::string &hostname, const std::string &port, int &timelimit, SickGenericParser *parser, char cola_dialect_id);
 
-  /// Send a SOPAS command to the device and print out the response to the console.
-  virtual int sendSOPASCommand(const char* request, std::vector<unsigned char> * reply);
+    virtual ~SickScanCommonTcp();
 
-  /// Read a datagram from the device.
-  /**
-   * \param [in] receiveBuffer data buffer to fill
-   * \param [in] bufferSize max data size to write to buffer (result should be 0 terminated)
-   * \param [out] actual_length the actual amount of data written
-   */
-  virtual int get_datagram(unsigned char* receiveBuffer, int bufferSize, int* actual_length);
- 
-  // Helpers for boost asio
-  int readWithTimeout(size_t timeout_ms, char *buffer, int buffer_size, int *bytes_read = 0, bool *exception_occured = 0);
-  void handleRead(boost::system::error_code error, size_t bytes_transfered);
-  void checkDeadline();
+    static void readCallbackFunctionS(void *obj, UINT8 *buffer, UINT32 &numOfBytes);
 
-private:
-  boost::asio::io_service io_service_;
-  boost::asio::ip::tcp::socket socket_;
-  boost::asio::deadline_timer deadline_;
-  boost::asio::streambuf input_buffer_;
-  boost::system::error_code ec_;
-  size_t bytes_transfered_;
+    void readCallbackFunction(UINT8 *buffer, UINT32 &numOfBytes);
 
-  std::string hostname_;
-  std::string port_;
-  int timelimit_;
+    void setReplyMode(int _mode);
+
+    int getReplyMode();
+
+
+	SopasEventMessage findFrameInReceiveBuffer();
+	void processFrame(SopasEventMessage& frame);
+	Queue<std::vector<unsigned char> > recvQueue;
+    UINT32 m_alreadyReceivedBytes;
+    UINT32 m_lastPacketSize;
+    UINT8  m_packetBuffer[480000];
+/**
+ * Read callback. Diese Funktion wird aufgerufen, sobald Daten auf der Schnittstelle
+ * hereingekommen sind.
+ */
+
+    protected:
+        void disconnectFunction();
+
+		void readCallbackFunctionOld(UINT8* buffer, UINT32& numOfBytes);
+		virtual int init_device();
+
+        virtual int close_device();
+
+        /// Send a SOPAS command to the device and print out the response to the console.
+        virtual int sendSOPASCommand(const char *request, std::vector<unsigned char> *reply, int cmdLen);
+
+        /// Read a datagram from the device.
+        /**
+         * \param [in] receiveBuffer data buffer to fill
+         * \param [in] bufferSize max data size to write to buffer (result should be 0 terminated)
+         * \param [out] actual_length the actual amount of data written
+         * \param [in] isBinaryProtocol true=binary False=ASCII
+         */
+        virtual int
+        get_datagram(unsigned char *receiveBuffer, int bufferSize, int *actual_length, bool isBinaryProtocol);
+
+        // Helpers for boost asio
+        int readWithTimeout(size_t timeout_ms, char *buffer, int buffer_size, int *bytes_read = 0,
+                            bool *exception_occured = 0, bool isBinary = false);
+
+        void handleRead(boost::system::error_code error, size_t bytes_transfered);
+
+        void checkDeadline();
+
+    private:
+
+
+
+		// Response buffer
+		UINT32 m_numberOfBytesInResponseBuffer; ///< Number of bytes in buffer
+		UINT8 m_responseBuffer[1024]; ///< Receive buffer for everything except scan data and eval case data.
+		Mutex m_receiveDataMutex; ///< Access mutex for buffer
+
+								  // Receive buffer
+		UINT32 m_numberOfBytesInReceiveBuffer; ///< Number of bytes in buffer
+		UINT8 m_receiveBuffer[480000]; ///< Low-Level receive buffer for all data 
+
+		bool m_beVerbose;
+
+        boost::asio::io_service io_service_;
+        boost::asio::ip::tcp::socket socket_;
+        boost::asio::deadline_timer deadline_;
+        boost::asio::streambuf input_buffer_;
+        boost::system::error_code ec_;
+        size_t bytes_transfered_;
+
+        std::string hostname_;
+        std::string port_;
+        int timelimit_;
+        int m_replyMode;
 };
 
-inline void SickScanCommonTcp::handleRead(boost::system::error_code error, size_t bytes_transfered)
-{
-    ec_ = error;
-    bytes_transfered_ += bytes_transfered;
-}
 
 } /* namespace sick_scan */
 #endif /* SICK_TIM3XX_COMMON_TCP_H */
