ae7566b6272f4f1d6ed3bbf50022c81f35ba0631
Michael Lehning <michael.lehning@lehning.de>
2019-02-14 17:30:04 +0100

    Rebase from 'release/melodic/sick_scan'

--- driver/src/sick_generic_caller.cpp
+++ driver/src/sick_generic_caller.cpp
@@ -98,11 +98,13 @@
 // 001.002.011: Ip Address change added, bugfixing
 // 001.003.000: Jan 2019  release 0.0.14
 // 001.003.001: Jan 2019  release 0.0.14 address handling for ip v4 parsing fixed
+// 001.003.002: Feb 2019 Fixing and optimizing console output
+// 001.003.016: Feb 2019 Profiling+instructions, Caching of Ros-Params
 
 
 #define SICK_GENERIC_MAJOR_VER "001"
 #define SICK_GENERIC_MINOR_VER "003"
-#define SICK_GENERIC_PATCH_LEVEL "001"
+#define SICK_GENERIC_PATCH_LEVEL "016"
 
 #include <algorithm> // for std::min
 
